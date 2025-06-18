f6aa75bfb80d933e6ec77cfc3c8f3d70180c7ae2
Heng <hengnix233@gmail.com>
2025-06-18 16:07:47 +0800

    feat: add GitHub Pages deployment workflow and update package scripts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === 'production'
+      ? '/health_management_frontend/'
+      : '/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
